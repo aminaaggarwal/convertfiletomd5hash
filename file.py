982d75ad6e186360a43d5c758a6b7dbba496efc3
aminaaggarwal <67671307+aminaaggarwal@users.noreply.github.com>
2021-10-04 14:05:40 +1300

    Update file.py

--- file.py
+++ file.py
@@ -1,9 +1,12 @@
+#Python hashlib hashing function takes variable length of bytes and converts it into a fixed length sequence.
 import hashlib
+#Variable defined with a max blocksize or the file size
 BLOCKSIZE = 65536
-hasher = hashlib.md5()
+#use md5 function from the haslib and save the value in the hasher variable 
+hashit = hashlib.md5()
 with open('filedirectorypath', 'rb') as afile:
     buf = afile.read(BLOCKSIZE)
     while len(buf) > 0:
-        hasher.update(buf)
+        hashit.update(buf)
         buf = afile.read(BLOCKSIZE)
-print(hasher.hexdigest())
+print(hashit.hexdigest())
